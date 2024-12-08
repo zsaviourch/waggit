92a8d34c7f9f21731789e168dc0f816820372bf8
zsaviourch <zsaviour@character.xyz>
2024-12-08 18:13:46 +0530

    Added bitcoin timer to home and updated UX colors for bg canvas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,56 +3,36 @@
 import React, { useEffect } from "react";
 import { Scene, Entity } from "aframe-react";
 import "aframe";
-import { PixelArtBitcoin } from "@/app/components/pixel-art-bitcoin";
-import { InteractiveGraph } from "./components/interactive-graph";
-import { BitcoinLiveSampleData } from "./data/bitcoin-live-sample-data";
+import { BitcoinTimer } from "./components/bitcoin-timer";
 
 const Home = () => {
-  useEffect(() => {
-    // Ensure the A-Frame scene is set up and rotating the cube
-    const box = document.querySelector("a-box");
-    let rotation = 0;
-
-    if (box) {
-      // Set up an interval to rotate the cube
-      const interval = setInterval(() => {
-        rotation += 0.5;
-        box.setAttribute("rotation", `0 ${rotation} 0`);
-      }, 100); // Update rotation every 100ms
-
-      // Cleanup on unmount
-      return () => clearInterval(interval);
-    }
-  }, []);
-
   return (
     <div>
+      {/* Add a gradient background using CSS */}
+      <div className="background-gradient" />
+
       <div className="aframe-container">
         <Scene
           embedded
           renderer="colorManagement: true"
           vr-mode-ui="enabled: false"
         >
-          <PixelArtBitcoin
-            position="-5 0 -5"
-            pixelArtBitcoinProps={{
-              pixelSize: 0.05,
-              color: "#F7931A",
-              shineColor: "white",
-              shineOpacity: 0.4,
-            }}
-          />
-
-          <InteractiveGraph position="-5 -2 -5" data={BitcoinLiveSampleData} />
-
-          <Entity primitive="a-sky" color="#ECECEC" />
+          {/* Example content in the scene */}
+          <BitcoinTimer position="0 2 -5" bitcoinValue="0.05" duration={600} />
         </Scene>
       </div>
 
       <style jsx>{`
-        h1 {
-          text-align: center;
+        .background-gradient {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          z-index: -1; /* Ensure it's behind the scene */
+          background: linear-gradient(180deg, #1e1e2f, #292946);
         }
+
         .aframe-container {
           width: 100%;
           height: 100vh;
