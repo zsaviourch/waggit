d95c9961625bdd47c686cde95eef2e9892740d8c
zsaviourch <zsaviour@character.xyz>
2024-12-08 16:15:05 +0530

    Added declaration for aframe and three

---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+
+// global.d.ts
+declare module "three";
